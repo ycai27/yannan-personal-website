6991ccd9ffa2abaf5d55c9f7fab00ca4d06061a1
Yannan Cai <yannancai27@gmail.com>
2026-07-06 23:24:57 -0700

    Personalize footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,12 +70,10 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
-          <TextLoop className="text-xs text-zinc-500">
-            <span>© 2024 Nim.</span>
-            <span>Built with Motion-Primitives.</span>
-          </TextLoop>
-        </a>
+        <TextLoop className="text-xs text-zinc-500">
+          <span>© {new Date().getFullYear()} Yannan Cai.</span>
+          <span>Built with Nim by ibelick.</span>
+        </TextLoop>
         <div className="text-xs text-zinc-400">
           <ThemeSwitch />
         </div>
